e4b8e72a8d755001ae26de6411bc2c14841b2ea9
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-04-12 12:26:01 +0200

    loading state before dashboard, and nav guards added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import { RouterView } from 'vue-router'
+import { state } from '@/modules/states/state';
 import Navbar from '@/components/nav-bar.vue'
 import footerElement from '@/components/footer-element.vue';
+
+
+const isLoading = computed(() => state.isLoading);
 </script>
 
 <template>
-  <Navbar/>
-  <RouterView />
-  <footerElement/>
+  <Navbar />
+  <!-- Loading -->
+  <div v-if="isLoading">Loading...</div>
+  <RouterView v-else />
+  <footerElement />
 </template>
 
 <style scoped></style>
--- src/modules/states/state.ts
+++ src/modules/states/state.ts
@@ -4,7 +4,12 @@ const isLoggedInLS: boolean = localStorage.getItem('isLoggedIn') === 'true';
 
 export const state = reactive({
    isLoggedIn: isLoggedInLS,
+   isLoading: false,
 });
+
+export const setLoadingState = (loading: boolean) => {
+   state.isLoading = loading;
+};
 
 watch(() => state.isLoggedIn, (newValue: boolean) => {
    localStorage.setItem('isLoggedIn', String(newValue));
