ddbbe7779a9c17f9f24bfcfdef35473b8100c503
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-04-16 11:31:11 +0200

    state commented out

--- src/modules/auth/useUsers.ts
+++ src/modules/auth/useUsers.ts
@@ -1,6 +1,5 @@
 import { ref } from "vue";
 import type { User } from "@/interfaces/interfaces"
-import { state } from "@/modules/states/state"
 
 export const useUsers = () => {
   const token = ref<string | null>(localStorage.getItem('lsToken'))
@@ -31,7 +30,7 @@ export const useUsers = () => {
       const authResponse = await response.json()
       token.value = authResponse.data.token
       await getUserById(authResponse.data.userId)
-      state.isLoggedIn = true
+      /* state.isLoggedIn = true */
 
       localStorage.setItem('lsToken', authResponse.data.token)
       localStorage.setItem('userId', authResponse.data.userId)
@@ -40,7 +39,7 @@ export const useUsers = () => {
     }
     catch (err) {
       error.value = (err as Error).message || 'Failed to login';
-      state.isLoggedIn = false;
+      /* state.isLoggedIn = false; */
     }
   }
 
@@ -62,7 +61,7 @@ export const useUsers = () => {
       const authResponse = await response.json()
       token.value = authResponse.data.token
       user.value = authResponse.data.user
-      state.isLoggedIn = true
+      /* state.isLoggedIn = true */
 
       localStorage.setItem('lsToken', authResponse.data.token)
       console.log('user is registered', authResponse)
@@ -70,7 +69,7 @@ export const useUsers = () => {
     }
     catch (err) {
       error.value = (err as Error).message
-      state.isLoggedIn = false
+      /* state.isLoggedIn = false */
     }
   }
 
@@ -118,18 +117,18 @@ export const useUsers = () => {
   const logout = () => {
     token.value = null
     user.value = null
-    state.isLoggedIn = false
+    /* state.isLoggedIn = false */
     localStorage.removeItem('lsToken')
     localStorage.removeItem('userId');
     console.log('user is logged out')
-    console.log("state: ", state.isLoggedIn)
+    /* console.log("state: ", state.isLoggedIn) */
     console.log("token: ", token.value)
     console.log("user: ", user.value)
   }
 
   return {
     token,
-    isLoggedIn: state.isLoggedIn,
+    /* isLoggedIn: state.isLoggedIn, */
     error,
     user,
     firstName,
