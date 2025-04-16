1eaeb0c041a9f13ea66ec9fb0788f59e18957e5b
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-04-16 11:29:55 +0200

    placeStore with fetch and filter for approved added with interface

---
+++ src/stores/crud/placesStore.ts
@@ -0,0 +1,60 @@
+import { defineStore } from "pinia";
+import { ref } from 'vue';
+import type { Place } from "@/interfaces/interfaces";
+
+export const usePlacesStore = defineStore('placesStore', {
+   state: () => ({
+      places: ref<Place[]>([]),
+      error: ref<string | null>(null),
+      isPlacesLoaded: ref<boolean>(false),
+      isLoading: ref<boolean>(false),
+   }),
+
+   actions: {
+      async fetchPlaces(): Promise<void> {
+         // Check if places are already loaded or loading to avoid multiple API calls
+         if (this.isPlacesLoaded || this.isLoading) {
+            console.log('Places already loaded or loading, skipping fetch')
+            return;
+         }
+
+         this.isLoading = true;
+
+         try {
+            console.log('Fetching places data...')
+            const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/places`, {
+               method: 'GET',
+            })
+
+            if (!response.ok) {
+               const errorResponse = await response.json()
+               throw new Error(errorResponse.error || 'No data available');
+            }
+
+            const placesData = await response.json()
+            this.places = placesData.data;
+
+            this.error = null;
+            this.isPlacesLoaded = true;
+         }
+         catch (err) {
+            this.error = (err as Error).message;
+            this.isPlacesLoaded = false;
+         }
+         finally {
+            this.isLoading = false;
+         }
+      },
+
+      filterPlacesByApproved(approved: boolean): Place[] {
+         if (this.places.length === 0) {
+            return []
+         }
+         return this.places.filter((place) => place.approved === approved)
+      }
+   },
+
+   getters: {
+
+   }
+})
