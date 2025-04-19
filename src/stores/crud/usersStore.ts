ded0863f142452e32559cc351dd96049b2cd7b9d
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-04-19 13:12:00 +0200

    userStore with fetchUsers, roleStore with fetchRoles, and fetchRolesById added

--- src/stores/crud/usersStore.ts
+++ src/stores/crud/usersStore.ts
@@ -10,7 +10,46 @@ export const useUsersStore = defineStore('usersStore', {
       isLoading: ref<boolean>(false),
    }),
 
-   actions: {
 
+   actions: {
+      async fetchUsers(): Promise<void> {
+         if (this.isUsersLoaded || this.isLoading) return // Prevents multiple calls to the API
+
+         this.isLoading = true;
+
+         try {
+            const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/users`, {
+               method: 'GET'
+            })
+
+            if (!response.ok) {
+               const errorResponse = await response.json()
+               throw new Error(errorResponse.error || 'No data available');
+            }
+
+            const usersData = await response.json()
+            this.users = usersData.data;
+            this.error = null;
+            this.isUsersLoaded = true;
+
+            console.log('Users fetched successfully:', this.users)
+         }
+         catch (err) {
+            this.error = (err as Error).message
+            this.users = [];
+            this.isUsersLoaded = false;
+         }
+         finally {
+            this.isLoading = false;
+         }
+
+      }
    },
+
+   getters: {
+      getUsers: (state) => state.users,
+      getError: (state) => state.error,
+      getIsUsersLoaded: (state) => state.isUsersLoaded,
+      getIsLoading: (state) => state.isLoading,
+   }
 })
