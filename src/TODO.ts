f21e1135b123fe0e37b6a8d6905d566a1f9d8b7e
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-04-24 13:19:22 +0200

    proper location display with conditions

--- src/TODO.ts
+++ src/TODO.ts
@@ -1,11 +1,5 @@
 //== ADMIN ==
 /**
- * Place Requests
- * - Country and City select from API dropdown (or user input but validating with API if it is a real country/city)
- * 
- * Places 
- * - Country and City select from API dropdown (or user input but validating with API if it is a real country/city)
- * 
  * City Display
  * -City image add, edit, delete
  * 
