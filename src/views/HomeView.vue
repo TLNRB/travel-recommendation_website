6c7a2e249aa1c86ab9b14a627d0989ed88ab22d2
bartekfenicki <bartek.fenicki@gmail.com>
2025-04-27 14:31:01 +0200

    search and most rated places
    issues - back button on places when routing from home
    fucking external api problems with data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,53 +1,8 @@
-<script setup lang="ts">
-import { ref, onMounted } from 'vue'
-// Stores
-import { usePlacesStore } from '@/stores/crud/placesStore';
-
-const activeTab = ref<'destinations' | 'atractions'>('destinations');
-const activeTabTwo = ref<'beaches' | 'museums' | 'monuments' | 'other'>('beaches');
-
-const placesStore = usePlacesStore();
-
-onMounted(() => {
-  placesStore.fetchPlaces();
-})
-</script>
-
 <template>
   <main>
     <!-- Search Section -->
-    <div class="container-fluid bg-gray-100 mx-auto px-4 sm:px-16 lg:px-64 xl:px-80 2xl:px-96 py-8">
-        <!-- Search  -->
-      <div class="mb-8 w-full">
-        <div class="flex gap-4 justify-start mb-4">
-          <span class="cursor-pointer px-3 py-1 ">All</span>
-          <span class="cursor-pointer px-3 py-1">Museums</span>
-          <span class="cursor-pointer px-3 py-1 rounded">Monuments</span>
-          <span class="cursor-pointer px-3 py-1">Beaches</span>
-          <span class="cursor-pointer px-3 py-1">etc.</span>
-        </div>
-        <div class="w-full">
-          <input
-            type="text"
-            placeholder="Search..."
-            class="w-full px-4 py-3 border rounded-2xl shadow-md"
-          />
-        </div>
-      </div>
+    <searchAndPopular/>
 
-      <!-- Most Rated -->
-      <div>
-        <h2 class="text-xl font-semibold mb-4">Most Rated</h2>
-        <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
-          <div class="w-full h-40 bg-gray-200 rounded-lg"></div>
-          <div class="w-full h-40 bg-gray-200 rounded-lg"></div>
-          <div class="w-full h-40 bg-gray-200 rounded-lg"></div>
-          <div class="w-full h-40 bg-gray-200 rounded-lg"></div>
-          <div class="w-full h-40 bg-gray-200 rounded-lg"></div>
-          <div class="w-full h-40 bg-gray-200 rounded-lg"></div>
-        </div>
-      </div>
-    </div>
 
     <!-- Tabs for the Top Section -->
     <div class="container-fluid flex justify-start ps-4 gap-2 bg-gray-100">
@@ -175,3 +130,18 @@ onMounted(() => {
      </div>
   </main>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+// Stores
+import { usePlacesStore } from '@/stores/crud/placesStore';
+import searchAndPopular from '@/components/home/searchAndPopular.vue'
+const activeTab = ref<'destinations' | 'atractions'>('destinations');
+const activeTabTwo = ref<'beaches' | 'museums' | 'monuments' | 'other'>('beaches');
+
+const placesStore = usePlacesStore();
+
+onMounted(() => {
+  placesStore.fetchPlaces();
+})
+</script>
